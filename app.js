f264e2b3a82e6ff2321855663f35ef177e3e15f9
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-06-18 22:13:12 +0530

    added some code for port and host and testing

--- app.js
+++ app.js
@@ -2,3 +2,32 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 3030;
+
+app.use((req,res,next)=>{
+    next();
+})
+
+app.get("/",(req,res)=>{
+    console.log(req.headers)
+    res.sendFile( __dirname + "/index.html" )
+})
+app.get("/getJson",(req,res)=>{
+    console.log(req.headers)
+    res.send({
+        name: "ADNAN KHAN",
+        post: "Software engineer",
+    })
+})
+
+
+app.listen(port);
+console.log(`running at port : ${port}`)
+
+var host  = Object.values(require('os').networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family==='IPv4' && !i.internal && i.address || []), [])), [])
+
+console.log("your localhost is : "+ host);
+console.log("for access on other devices (on same network) : "+ host +":"+port+"/");
