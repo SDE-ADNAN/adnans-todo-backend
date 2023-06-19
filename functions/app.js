188e4b136cb6f9792f4dfdc2ba35afd9439f24ea
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-06-19 07:59:47 +0530

    netlify deployment

--- functions/app.js
+++ functions/app.js
@@ -4,37 +4,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const router = express.Router();
+const serverless = require('serverless-http');
 
 const port = 3030;
 
-const cors = require('cors');
 
-// ...
-
-app.use(cors());
-
-
-app.use((req,res,next)=>{
+router.use((req,res,next)=>{
     next();
 })
 
-app.get("/",(req,res)=>{
+router.get("/",(req,res)=>{
     console.log(req.headers)
     res.status(200).send({
         name: "ADNAN KHAN",
         post: "Software engineer",
     })
 })
-app.get("/getJson",(req,res)=>{
+router.get("/getJson",(req,res)=>{
     console.log(req.headers)
     res.sendFile( __dirname + "/index.html" )
 })
 
 
-app.listen(port);
-console.log(`running at port : ${port}`)
+// app.listen(port);
+// console.log(`running at port : ${port}`)
 
 var host  = Object.values(require('os').networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family==='IPv4' && !i.internal && i.address || []), [])), [])
 
 console.log("your localhost is : "+ host);
 console.log("for access on other devices (on same network) : "+ host +":"+port+"/");
+
+
+app.use('/.netlify/functions/app', router);
+module.exports.handler = serverless(app);
