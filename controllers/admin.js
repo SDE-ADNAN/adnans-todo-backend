266c20b71011808582b94cd355792b5504757ee7
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-06-19 14:24:21 +0530

    added a get todos admin controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,15 @@
+const Todo = require("../models/todo");
 const path = require("../utils/path");
 
 
 
 exports.getJson = (req, res, next) => {
-    res.send({
-        name:"adnan",
-        path:path,
-        desiredLPA:"55LPA"
-    });
-    res.redirect('/')
+    const newTodo = new Todo("call Papa", [] , true , false , false  ,false)
+    console.log(newTodo)
+    newTodo.save("1687164765261_33137");
+    Todo.fetchAll((todos)=>{
+        res.json(todos)
+    })
+    // res.redirect('/')
+    
   };
