90a2d82237ad05e77942a71300ca0992d039d5f1
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-06-20 18:45:00 +0530

    fix3

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,5 +21,5 @@ exports.postTodo = (req, res, next) => {
     //     res.json(todos)
     // })
     console.log(JSON.stringify(req.body))
-    res.json(req.body)
+    res.status(200).json(req.body)
   };
