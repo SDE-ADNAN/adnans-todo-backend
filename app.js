98f77296d8e650655f2fe7481609555a795c938f
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-06-20 18:31:44 +0530

    figured out the issue for form-data

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const path = require('path');
-const path2 = require("./utils/path")
+const path2 = require("./utils/path");
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 const adminRoutes = require('./routes/admin');
-const app = express();
 const cors = require('cors');
+const multer = require('multer');
+const app = express();
 
 dotenv.config({
     path:"./ENV/config.env"
 })
+
 app.use(cors());
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json())
+const upload = multer(); // multer instance
+
+app.use(upload.any()); // middleware to handle form-data
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is working on Port ${process.env.PORT}`)
-    console.error(path2)
 })
 
 var host  = Object.values(require('os').networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family==='IPv4' && !i.internal && i.address || []), [])), [])
