86adf3e095f21b960873cf30245c33c74f247d98
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-06-22 21:43:43 +0530

    used colors package

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,12 @@
 const Todo = require("../models/todo");
 const path = require("../utils/path");
+// const chalk = require('chalk');
+const colors = require('colors');
 
 
 
 exports.getAllTodos = (req, res, next) => {
+    console.log("///////////newTodo////////////////".yellow)
     Todo.fetchAll((todos)=>{
         res.json(todos)
     })
@@ -13,8 +16,8 @@ exports.getAllTodos = (req, res, next) => {
 exports.postTodo = (req, res, next) => {
     const {parentId,title} = req.body
     const newTodo = new Todo(title, [] , true , false,false,true)
-    console.log("///////////newTodo////////////////")
-    console.log(newTodo)
+    console.log("///////////newTodo////////////////".yellow)
+    console.log(colors.red.underline(newTodo))
     newTodo.save(parentId?parentId:"");
     console.log("///////////REQ.BODY////////////////")
     console.log(JSON.stringify(req.body))
