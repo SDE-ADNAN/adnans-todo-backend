466aa5238a8672f35e370eb8462b05a1d77f6706
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-06-22 21:42:55 +0530

    attempt to create a logger

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const path2 = require("./utils/path");
 const express = require('express');
+// const logger = require('./logger')
+const pjson = require('./package.json')
 
 const bodyParser = require('body-parser');
 const multer = require('multer');
@@ -24,14 +26,11 @@ app.use(bodyParser.json())
 
 app.use('/admin', adminRoutes);
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is working on Port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT)
 
 var host  = Object.values(require('os').networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family==='IPv4' && !i.internal && i.address || []), [])), [])
 
 console.log("your localhost is : "+ host);
 console.log("for access on other devices (on same network) : "+ host +":"+process.env.PORT+"/");
 
-
-
+// logger.log.info(`${pjson.name}`)
