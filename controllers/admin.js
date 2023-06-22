0057b5483b941e7ac68a7cded75ee9e06b0bcc2c
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-06-22 19:59:45 +0530

    changed the default initial newTodo properties

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,9 +4,6 @@ const path = require("../utils/path");
 
 
 exports.getAllTodos = (req, res, next) => {
-    // const newTodo = new Todo("call Papa", [] , true , false , false  ,false)
-    // console.log(newTodo)
-    // newTodo.save("1687164765261_33137");
     Todo.fetchAll((todos)=>{
         res.json(todos)
     })
@@ -14,14 +11,11 @@ exports.getAllTodos = (req, res, next) => {
 
 
 exports.postTodo = (req, res, next) => {
-    const {parentId,title,isCreated,showInput,isCompleted,showSubtodos} = req.body
-    const newTodo = new Todo(title, [] , isCreated==="true" , showInput==="true" , isCompleted==="true"  ,showSubtodos==="true")
+    const {parentId,title} = req.body
+    const newTodo = new Todo(title, [] , true , false,false,true)
     console.log("///////////newTodo////////////////")
     console.log(newTodo)
     newTodo.save(parentId?parentId:"");
-    // Todo.fetchAll((todos)=>{
-    //     res.json(todos)
-    // })
     console.log("///////////REQ.BODY////////////////")
     console.log(JSON.stringify(req.body))
     res.status(200).json(req.body)
