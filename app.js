f8545356be1f7e0abbcd96e8125b8c131431c57a
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-06-25 00:22:43 +0530

    imported and used logger

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const path = require('path');
-const path2 = require("./utils/path");
 const express = require('express');
-// const logger = require('./logger')
-const pjson = require('./package.json')
-
 const bodyParser = require('body-parser');
 const multer = require('multer');
-
 const dotenv = require("dotenv");
 const adminRoutes = require('./routes/admin');
 const cors = require('cors');
+const logger = require("./logger/index")
+
+
 const app = express();
 
 dotenv.config({
@@ -30,7 +27,11 @@ app.listen(process.env.PORT)
 
 var host  = Object.values(require('os').networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family==='IPv4' && !i.internal && i.address || []), [])), [])
 
-console.log("your localhost is : "+ host);
-console.log("for access on other devices (on same network) : "+ host +":"+process.env.PORT+"/");
+logger.info("your localhost is : "+ host);
+logger.info("for access on other devices (on same network) : "+ host +":"+process.env.PORT+"/");
+logger.info("hhhhhhhhhhhhhhhhhhhhh")
+
+logger.error("hi from error")
+logger.warn("hi from warn")
 
 // logger.log.info(`${pjson.name}`)
