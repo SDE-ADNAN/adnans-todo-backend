7fd19bb57621dfabdeb991e318b916f9ceeb4eab
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-06-24 22:50:01 +0530

    added the  /getTodo route

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,7 @@ const adminController = require('../controllers/admin');
 const router = express.Router();
 
 router.get('/getAllTodos', adminController.getAllTodos);
+router.get('/getTodo', adminController.getTodo);
 router.post('/postTodo', adminController.postTodo);
 
 module.exports = router;
