c460008490d8969089da5e0e14eb611de7579ff2
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-26 14:36:51 +0530

    authmiddleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+
+const authenticateUser =(req,res,next)=>{
+    try{
+        const token = req.header('Authorization')?.replace('Bearer ','');
+        if(!token){
+            return res.status(401).json({ message:'Authentication token missing.'});
+        }
+
+        const decoded = jwt.verify(token,process.env.SECRET);
+        req.userId = decoded.userId;
+        next()
+    }catch(error){
+        console.error('Error authenticating user: ',error);
+        return res.status(401).json({message:'Invalid authentication token. '})
+    }
+}
+
+module.exports = authenticateUser;
