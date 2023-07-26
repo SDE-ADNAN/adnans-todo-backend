2fa4207b0e12b670b99e562c3693438300fe4c51
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-26 09:33:56 +0530

    rename

--- models/todo.js
+++ models/todo.js
@@ -1,161 +1,31 @@
-const fs = require('fs');
-const path = require('path');
-const logger = require('../logger/index')
+const mongoose = require('mongoose');
 
-const p = path.join(
-  path.dirname(require.main.filename),
-  'data',
-  'todos.json'
-);
-
-function generateUniqueId() {
-    const timestamp = new Date().getTime();
-    const randomNumber = Math.floor(Math.random() * 100000);
-    return `${timestamp}_${randomNumber}`;
-  }
-
-const getTodosFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
-
-const findTodoById = (id, todos)=> {
-    for (const todo of todos) {
-      if (todo.id === id) {
-        return todo;
-      } else if (todo.todo.length > 0) {
-        const foundTodo = findTodoById(id, todo.todo);
-        if (foundTodo) {
-          return foundTodo;
+const todoSchema = new mongoose.Schema({
+    title:{
+        type:String,required:true,
+    },
+    todo:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'SubTodo',
         }
-      }
+    ],
+    isCreated: {
+        type: Boolean,
+        default: false
+    },
+    showInput: {
+        type: Boolean,
+        default: false
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    },
+    showSubtodos: {
+        type: Boolean,
+        default: false
     }
-    return undefined;
-  };
+})
 
-  const deleteTodoById = (id , todos) => {
-    return todos.filter(todo => {
-      if (todo.id === id) {
-        // Exclude the todo with the given id
-        return false;
-      } else if (todo.todo.length > 0) {
-        // Recursively delete todos from the sub-todo list
-        todo.todo = deleteTodoById(id, todo.todo);
-        return true;
-      } else {
-        // Include the todo if it's not the one to be deleted
-        return true;
-      }
-    });
-  };
-
-
-  //Helper for changing a todo at a specific id
-const changeTodoById = (id, updatedTodo, todos) => {
-  return todos.map(todo => {
-    if (todo.id === id) {
-      return updatedTodo;
-    } else if (todo.todo.length > 0) {
-      return { ...todo, todo: changeTodoById(id, updatedTodo, todo.todo) };
-    } else {
-      return todo;
-    }
-  });
-};
-
-const getCurrDateAndTime=()=>{
-  var date = new Date();
-  var dateStr =
-    ("00" + (date.getMonth() + 1)).slice(-2) + "/" +
-    ("00" + date.getDate()).slice(-2) + "/" +
-    date.getFullYear() + " " +
-    ("00" + date.getHours()).slice(-2) + ":" +
-    ("00" + date.getMinutes()).slice(-2) + ":" +
-    ("00" + date.getSeconds()).slice(-2);
-    return  dateStr
-}
-
-  /**
-  * @name Todo
-  * @param { object } object - { todoDetails } parameter
-  * @returns { object } object - { newTodo } 
-  * `{ title: todoDetails.title,
-  * todo: todoDetails.todo,
-    isCreated: todoDetails.isCreated,
-    showInput: todoDetails.showInput,
-    isCompleted: todoDetails.isCompleted,
-    showSubtodos: todoDetails.showSubtodos, }`
-  * @description Takes title , todo , isCreated , showInput, isCompleted, showSubtodos as args and creates a new todo , but doesn't saves it , u have to save it via `netTodo.save()` .
-  */
-module.exports = class Todo {
-
-  constructor(title, todo, isCreated, showInput,isCompleted,showSubtodos) {
-    this.title= title;
-    this.todo= todo;
-    this.isCreated= isCreated;
-    this.showInput= showInput;
-    this.isCompleted=isCompleted;
-    this.showSubtodos=showSubtodos;
-  }
-
-  save(parentId) {
-    this.id= generateUniqueId()
-    this.createdOn = getCurrDateAndTime()
-    getTodosFromFile(todos => {
-      const parentTodo = findTodoById(parentId, todos);
-      logger.info(parentTodo)
-      if (parentTodo) {
-        parentTodo.todo.unshift(this);
-      } else {
-        todos.unshift(this);
-      }
-      fs.writeFile(p, JSON.stringify(todos), err => {
-        logger.info(err);
-      });
-    });
-  }
-
-  static fetchAll(cb) {
-    getTodosFromFile(cb);
-  }
-
-  static findById(id, cb){
-    getTodosFromFile(todos => {
-      const todo = findTodoById(id,todos);
-      cb(todo);
-    })
-  }
-
-  static deleteTodoById = (id,cb) => {
-    getTodosFromFile(todos => {
-      const deletedTodo = findTodoById(id,todos);
-      // const deletedTodoCatched = JSON.parse(JSON.stringify(deletedTodo||{}))
-      const updatedTodos = deleteTodoById(id,todos);
-      fs.writeFile(p, JSON.stringify(updatedTodos), err => {
-        logger.error(err);
-      });
-      logger.warn(`Todo with ID : ${id} got DELETED`)
-      logger.warn(JSON.stringify(deletedTodo))
-      cb(updatedTodos, deletedTodo)
-    })
-  };
-
-  static updateTodoById (id ,changeObj, cb){
-    getTodosFromFile(todos => {
-      let updatedTodo = findTodoById(id,todos);
-      logger.warn("found id to update todo : "+id)
-      updatedTodo = {...updatedTodo , ...changeObj,editedOn:getCurrDateAndTime()}
-      const updatedTodos = changeTodoById(id ,updatedTodo,todos )
-      fs.writeFile(p, JSON.stringify(updatedTodos), err => {
-        logger.error(err);
-      });
-      logger.warn(JSON.stringify(updatedTodo))
-      cb(updatedTodo,updatedTodos);
-    })
-  }
-};
+module.exports = mongoose.model('Todo',todoSchema)
