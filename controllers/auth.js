c2af929aaacfbf17ab072cc5a6f27c06d563f915
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-07-29 02:02:07 +0530

    added headers check for doubly headers sent check

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,17 +13,17 @@ exports.registerUser = (req, res, next) => {
         .then((existingUser) => {
             if (existingUser) {
                 return res.status(409).json({ message: 'Username or email already exists. ' });
+            } else {
+                const newUser = new User({
+                    userName: userName,
+                    password: password,
+                    email: email,
+                    picUrl: picUrl,
+                    todos: [] // Initialized an empty array for todos
+                });
+
+                return newUser.save();
             }
-
-            const newUser = new User({
-                userName: userName,
-                password: password,
-                email: email,
-                picUrl: picUrl,
-                todos: [] // Initialized an empty array for todos
-            });
-
-            return newUser.save();
         })
         .then((newlyCreatedUser) => {
             if (newlyCreatedUser) {
@@ -40,29 +40,29 @@ exports.registerUser = (req, res, next) => {
 exports.loginUser = (req, res, next) => {
     const { userName, password } = req.body;
 
-    User.findOne(
-        { userName }
-    )
+    User.findOne({ userName })
         .then((user) => {
             if (!user) {
-                res.status(404).json({ message: 'User Not Found. ' })
+                return res.status(404).json({ message: 'User Not Found. ' });
             }
 
-            bcrypt.compare(password, user.password)
-                .then((err, result) => {
-                    if (err) {
-                        return res.status(401).json({ message: 'Invalid Credentials. ' })
-                    }
-                }).catch(err => console.error(err))
+            bcrypt.compare(password, user.password).then((result) => {
+                if (!result) {
+                    return res.status(401).json({ message: 'Invalid Credentials. ' });
+                }
 
-            const token = jwt.sign({ userId: user._id }, process.env.SECRET,)
-            return res.status(200).json({ message: 'Login successful. ', token })
+                const token = jwt.sign({ userId: user._id }, process.env.SECRET);
+                return res.status(200).json({ message: 'Login successful. ', token });
+            }).catch((err) => {
+                console.error(err);
+                return res.status(500).json({ message: 'Failed to compare passwords.' });
+            });
         })
-        .catch(err => {
+        .catch((err) => {
             console.error('Error logging in user: ', err);
-            return res.status(500).json({ message: 'Failed to Login user' })
-        })
-}
+            return res.status(500).json({ message: 'Failed to Login user' });
+        });
+};
 
 // Get user Profile (requires authentication)
 exports.getUserProfile = (req, res) => {
