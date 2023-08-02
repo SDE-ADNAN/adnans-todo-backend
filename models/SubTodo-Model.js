2bf49f9a0bae8fdc33703d648d72235bc658008e
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-08-02 11:42:07 +0530

    added description field

--- models/SubTodo-Model.js
+++ models/SubTodo-Model.js
@@ -4,6 +4,9 @@ const subTodoSchema = new mongoose.Schema({
     title: {
         type: String, required: true,
     },
+    description: {
+        type: String, required: true,
+    },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
